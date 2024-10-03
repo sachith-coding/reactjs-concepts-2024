696404f615f48fa4e928d6682156e7b7ed2ffe26
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2024-10-04 00:15:22 +0530

    Ternary Operator example

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import ProductItem from './components/ProductItem'
 import './style.css'
 
+
 const ProductList = ({ listOfProducts, name, city }) => {
+
+    const flag = false
+
+    function getTextBlock(flagVal)
+    {
+        return flagVal ? (<h3 className='title'>{name} is belongs to {city}</h3>) : (<h3>Flag val is false</h3>)
+    }
+
+
     return (
         <>
-            <h3 className='title'>{name} is belongs to {city}</h3>
+            {getTextBlock(flag)}
             <ul>
                 {
                     listOfProducts.map((item, index) => (
