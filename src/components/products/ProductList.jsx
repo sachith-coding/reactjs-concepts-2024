699000e8222c20caf2e7bd45bfd091fc584bad82
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2024-10-04 00:35:42 +0530

    Implement UseState Hook

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductItem from './components/ProductItem'
 import './style.css'
 
 
 const ProductList = ({ listOfProducts, name, city }) => {
 
-    const flag = false
+    const [flag, setFlag] = useState(true)
 
     function getTextBlock(flagVal)
     {
         return flagVal ? (<h3 className='title'>{name} is belongs to {city}</h3>) : (<h3>Flag val is false</h3>)
     }
 
+    const handleToggleText = () => {
+        setFlag(!flag)
+    }
 
     return (
         <>
+            <button onClick={handleToggleText}>Toggle Text</button>
             {getTextBlock(flag)}
             <ul>
                 {
