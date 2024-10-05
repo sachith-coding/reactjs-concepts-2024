d7031877a5ade96c2fbf97cc51df0cd806b39264
Sachith Priyashantha <sachithpriyashantha24@gmail.com>
2024-10-05 05:48:31 +0530

    Fetching Data from API

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductList from './components/products/ProductList'
+import Users from './components/users/users'
 
 const dummyProducts = ['Apple', 'Samsung', 'Huawei', 'Redmi']
 
@@ -12,7 +13,8 @@ function App() {
   return (
     <>
       <h1>ReactJS Concepts 2024</h1>
-      <ProductList listOfProducts={dummyProducts} name='Sachith' city='Anuradhapura' />
+      {/* <ProductList listOfProducts={dummyProducts} name='Sachith' city='Anuradhapura' /> */}
+      <Users />
     </>
   )
 }
---
+++ src/components/users/Users.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+const Users = () => {
+
+    const [userList, setUserList] = useState([]);
+    const [loading, setLoading] = useState(true)
+
+    async function fetchApiData() {
+        try
+        {
+            setLoading(true)
+            const apiData = await fetch('https://dummyjson.com/users');
+            const jsonData = await apiData.json()
+
+            if(jsonData?.users){
+                setUserList(jsonData?.users)
+                setLoading(false)
+            }
+            else{
+                setUserList([])
+                setLoading(false)
+            }
+        }
+        catch(ex)
+        {
+            console.log(ex);            
+        }
+    }
+
+    useEffect(() => {
+        fetchApiData()
+    }, [])
+
+    if(loading)
+        return (<p>Loading Data. Please wait!</p>)
+
+    return (
+        <div>
+            <h2>Fetch All Users</h2>
+            <ul>
+                {
+                    userList != null && userList.length > 0 
+                    ? userList.map((userItem) => (
+                        <li key={userItem?.id}>{userItem?.firstName} {userItem?.lastName}</li>
+                    ))
+                    : <p>No users found. Please try again.</p>
+
+
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Users
